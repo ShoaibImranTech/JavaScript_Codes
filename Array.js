a64c12dc29c81973a6758e696dc7b831cd7d8cb8
Muahmmad Shoaib <shoaibimran4569@gmail.com>
2025-08-17 04:30:30 -0700

    Add files via upload

---
+++ Array.js
@@ -0,0 +1,14 @@
+let Fruit=["MANGO","APPLE","ORANGE","PINEAPPLE","LITCHIE","BANANA"];
+console.log(Fruit);
+//Array By Index
+console.log(Fruit[0]);
+console.log(Fruit[1]);
+console.log(Fruit[2]);
+console.log(Fruit[3]);
+console.log(Fruit[4]);
+console.log(Fruit[5]);
+//Add Element in Array
+Fruit.push("Grapes");
+console.log(Fruit);
+//Get Element Which si added
+console.log(Fruit.pop());
---
+++ objects.js
@@ -0,0 +1,10 @@
+//objects
+let Fruit = {
+    name_fruit : 'mango',
+    Price_perKg : 100,
+    Offer : "Get 20% Off On 3kg",
+};
+console.log(Fruit);
+console.log(Fruit.Offer);
+console.log(Fruit.name_fruit);
+console.log(Fruit.Price_perKg);
