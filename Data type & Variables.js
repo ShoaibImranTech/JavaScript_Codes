ad58981cfc6a331a0902864d5273d88c94ea6910
Muahmmad Shoaib <shoaibimran4569@gmail.com>
2025-08-17 04:09:09 -0700

    Add files via upload

---
+++ Data type & Variables.js
@@ -0,0 +1,9 @@
+//Mutable Datatype
+let fruit= "Mango";
+let sport="Cricket";
+fruit = "orange";//Mutable
+console.log(fruit);
+console.log(sport);
+// Constant datatype which is Unmutable
+const class_dept="Bscs";
+console.log(class_dept);
